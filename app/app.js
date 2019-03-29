b9673d4c1dd1a9aed9f75c9b94cd937c3aa45957
mhf <mhfcbz@163.com>
2019-03-29 16:30:08 +0800

    s:

---
+++ app/app.js
@@ -0,0 +1,31 @@
+// import pizzaDiagram from '../resources/pizza-collaboration.bpmn';
+
+// import customElements from './custom-elements.json';
+
+import CustomModeler from './custom-modeler';
+
+/* function main() {
+  var modeler = new CustomModeler({
+    container: '#canvas',
+    keyboard: {
+      bindTo: document
+    }
+  });
+
+  modeler.importXML(pizzaDiagram, function(err) {
+
+    if (err) {
+      console.error('something went wrong:', err);
+    }
+
+    modeler.get('canvas').zoom('fit-viewport');
+
+    modeler.addCustomElements(customElements);
+  });
+}
+main() */
+
+window.BpmnJS = CustomModeler
+
+// expose bpmnjs to window for debugging purposes
+// window.bpmnjs = modeler;
